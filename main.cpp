2d17b8e6e7292f8736530560ce0648971ed8e87a
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-05-02 00:56:48 +0800

    新增 程序基础文件。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// main.cpp: 定义应用程序的入口点。
+//
+
+#include "header.h"
+
+using namespace std;
+
+int main()
+{
+	string logo = R"(  ____                        _         
+ |  _ \    __ _   _ __     __| |   __ _ 
+ | |_) |  / _` | | '_ \   / _` |  / _` |
+ |  __/  | (_| | | | | | | (_| | | (_| |
+ |_|      \__,_| |_| |_|  \__,_|  \__,_|
+                                        )";
+	cout << logo << endl;
+	return 0;
+}
